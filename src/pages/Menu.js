54050b4416a1e46b77539091d43fa1e4458e318d
Dainwi <danwichoudhary@gmail.com>
2023-03-04 14:54:26 +0530

    menu

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -8,7 +8,13 @@ const Menu = () => {
       style={{ margin: "1rem", display: "flex", justifyContent: "center" }}
     >
       <div className="container">
-        <Image src={"/menu.jpg"} alt="" width="1000" height="1200" />
+        <Image
+          src={"/menu.jpg"}
+          alt=""
+          width="1000"
+          height="1200"
+          style={{ overflow: "-moz-hidden-unscrollable" }}
+        />
       </div>
     </section>
   );
